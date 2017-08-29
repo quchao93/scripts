c9667511d7f20bcbc7af5628c574e69fef445475
chao.qu <chao.qu@nxp.com>
2017-08-29 20:41:55 +0800

    Update doxyfile_generator.py
    Signed-off-by: chao.qu <chao.qu@nxp.com>

--- doxyfile_generator2/doxyfile_generator.py
+++ doxyfile_generator2/doxyfile_generator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #Filename : doxyfile_generator2.py
+#This doxyfile_generator2 applies to release 7.
 
 import re, sys, argparse, os, yaml, msvcrt
 from feature_parser import FeatureParser
@@ -14,8 +15,6 @@ fixdir = '../../../'
 space = '                         '
 # The directory of components yml files
 dir_components = '/bin/generator/records/msdk/components/socs'
-# The directory of msdk
-dir_msdk = '/bin/generator/records/'
 #components file name
 file_components = 'components.yml'
 #template file name
@@ -25,6 +24,11 @@ changelog_doxyfile_template = 'Doxyfile_lib_PDF_ChangeLog'
 not_include_middle = ['emv', 'fatfs', 'lwip', 'mbedtls', 'multicore', 'usb', 'wolfssl']
 #suffix for multicore. Need to be update, if there are more suffix.
 suffix_for_multicore = ['', '_cm0plus', '_cm4', '_M0P', '_M4', '_CORE0', '_CORE1']
+#middleware list
+middleware_list_dic = {'aws_iot': ['middleware/aws_iot', 'middleware/aws_iot'], 'emv': ['', 'middleware/emv'], 'emwin': ['middleware/emwin', 'middleware/emwin'], 'fatfs': ['', 'middleware/fatfs'],
+'lwip': ['', 'middleware/lwip'], 'mbedtls': ['', 'middleware/mbedtls'], 'mmcau': ['middleware/mmcau', 'middleware/mmcau'], 'multicore': ['', 'middleware/multicore'],
+'ntag_i2c_plus': ['middleware/ntag_i2c_plus', 'middleware/ntag_i2c_plus'], 'sdmmc': ['middleware/sdmmc', 'middleware/sdmmc'], 'usb': ['', 'middleware/usb'], 'wifi_qca': ['middleware/wifi_qca', 'middleware/wifi_qca'], 'wolfssl': ['', 'middleware/wolfssl'], 'freertos': ['', 'middleware/rtos']}
+ 
 
 ## Read the options input by using the command line.
 def __read_options():
@@ -70,41 +74,50 @@ def __get_components_list(components_files_list):
     
     components_in_file = []
     components_dir_list = []
+    middleware_dir_list = []
     ##Get all components
-    for dir_file_components in components_files_list:
-        components_file = open(dir_file_components, 'r')
-        for line in components_file.readlines():
+    for component_file_dir in components_files_list:
+        component_file = open(component_file_dir, 'r')
+        for line in component_file.readlines():
             components_in_file.append(line)
-        components_file.close()
-    ##Filter duplicate    component    
+        component_file.close()
+    ##Filter duplicate component    
     all_components = set(components_in_file)
     for component in all_components:
-        if re.match(r'^- bin/generator/records/msdk/components/(cmsis_|)drivers/.*yml$', component) != None:
+        if re.match(r'^- bin/generator/records/msdk/components/(drivers|cmsis_drivers|utilities)/.*yml$', component) != None:
             searchobj = re.sub('^- ', '', component, 1)
             # searchobj = re.sub('/', r'\\', searchobj)
             searchobj = searchobj.strip('\n')
             components_dir_list.append(rootdir + '/' + searchobj)
-        # elif re.match(r'^- bin/generator/records/msdk/components/middlewares/.*yml$', component) != None:
-            # for middleware in not_include_middle:
-                # if re.match(middleware, component) == None:
-                    # searchobj = re.sub('^- ', '', component, 1)
-                    # searchobj = searchobj.strip('\n')
-                    # components_dir_list.append(rootdir + '/' + searchobj)
-        elif re.match(r'^- bin/generator/records/msdk/components/utilities/.*yml$', component) != None:
-            searchobj = re.sub('^- ', '', component, 1)
-            searchobj = searchobj.strip('\n')
-            components_dir_list.append(rootdir + '/' + searchobj)
-    return components_dir_list
+        elif re.search(r'^- bin/generator/records/msdk/components/middlewares/(.*?)(/.*yml$)', component) != None:
+            target_middleware = re.search(r'^- bin/generator/records/msdk/components/middlewares/(.*?)(/.*yml)', component).group(1)
+            # print component
+            print target_middleware
+            if function_type == 'rm':
+                if middleware_list_dic.has_key(target_middleware):
+                    if middleware_list_dic[target_middleware][0] != '':
+                        middleware_dir_list.append(middleware_list_dic[target_middleware][0])
+                else:
+                    print '\nWarning:  Not find middleware:' + target_middleware + '. Please update middleware_list_dic in script.'
+            elif function_type == 'changelog':
+                if middleware_list_dic.has_key(target_middleware):
+                    if middleware_list_dic[target_middleware][1] != '':
+                        middleware_dir_list.append(middleware_list_dic[target_middleware][1])
+                else:
+                    print '\nWarning:  Not find middleware:' + target_middleware + '. Please update middleware_list_dic in script.'
+    # print middleware_dir_list
+    return (components_dir_list, middleware_dir_list)
 
 #process driver.yml to get the driver path
-def __process_components_list(chip_name, components_list):
+def __process_components_list(chip_name, components_list, middleware_list):
     components_dir = []
-    component_name = ''
-    path = ''
     for component in components_list:
         # (filepath,tempfilename) = os.path.split(component)
         # (filename,extension) = os.path.splitext(tempfilename)
         # component_name = filename.replace('drv_', 'driver.')
+        # print component
+        component_name = ''
+        path = ''
         file = open(component, 'r')
         for line in file.readlines():
             if re.match('^(driver|utility|utilities).*:', line) != None:
@@ -115,19 +128,33 @@ def __process_components_list(chip_name, components_list):
         yml = yaml.load(file)
         # print component_name
         # print yml
-        path = yml[component_name]['contents']['cc-include'][0]['path']
+        try:
+            path = yml[component_name]['contents']['cc-include'][0]['path']
+        except KeyError:
+            try:
+                filename = yml[component_name]['contents']['files'][0]['source']
+                path = os.path.split(filename)[0];
+                # print path
+            except KeyError:
+                print "\nWarning: Not find path for :" + component_name
         if '${platform_devices_soc_name}' in path:
             path = yml[component_name]['contents']['files'][0]['source']
             path = path.replace('${platform_devices_soc_name}', chip_name)
         # print path
         components_dir.append(path)
-        #Remove duplicate elements
-        components_dir_list = list(set(components_dir))
-        components_dir_list.sort()
         file.close
+
+    #Add middleware dictory 
+    components_dir = components_dir + middleware_list
+    # components_dir.extend(middleware_list)
+    #Remove duplicate elements
+    components_dir_list = list(set(components_dir))
+    components_dir_list.sort()
+    # print '----------------------'
+    # print components_dir_list
     return components_dir_list
     
-#Add additional information, including clock.dox power.dox middleware utilities.
+#Add additional information, including clock.dox power.dox 
 def __add_input_information(chip_name, components_dir_list, feature_dic):
     #default add clock.dox path
     components_dir_list.append('platform/drivers/clock')
@@ -149,23 +176,30 @@ def __add_input_information(chip_name, components_dir_list, feature_dic):
     # components_dir_list.append('platform/utilities/shell')
     
     #Add middleware
-    if feature_dic.has_key('FSL_FEATURE_SOC_DMAMUX_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_DMA_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_EDMA_COUNT'):
-        components_dir_list.append('middleware/dma_manager')
-    if feature_dic.has_key('FSL_FEATURE_SOC_MMCAU_COUNT'):
-        components_dir_list.append('middleware/mmcau')
-    if feature_dic.has_key('FSL_FEATURE_SOC_SDHC_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_DSPI_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_SPI_COUNT'):
-        components_dir_list.append('middleware/sdmmc')
+    # if feature_dic.has_key('FSL_FEATURE_SOC_DMAMUX_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_DMA_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_EDMA_COUNT'):
+        # components_dir_list.append('middleware/dma_manager')
+    # if feature_dic.has_key('FSL_FEATURE_SOC_MMCAU_COUNT'):
+        # components_dir_list.append('middleware/mmcau')
+    # if feature_dic.has_key('FSL_FEATURE_SOC_SDHC_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_DSPI_COUNT') or feature_dic.has_key('FSL_FEATURE_SOC_SPI_COUNT'):
+        # components_dir_list.append('middleware/sdmmc')
     return components_dir_list
     
 def __format_input(all_inputdata):
     input_data = []
+    image_path = []
     for num, data in enumerate(all_inputdata):
         if (num == 0):
             pre_fixdir = 'INPUT                  = '
         else:
             pre_fixdir = space
         input_data.append(pre_fixdir + fixdir + data)
-    return input_data
+    for num, data in enumerate(all_inputdata):
+        if (num == 0):
+            pre_fixdir = 'IMAGE_PATH             = '
+        else:
+            pre_fixdir = space
+        image_path.append(pre_fixdir + fixdir + data)
+    return (input_data, image_path)
     
 def __format_feature(feature_dic):
     feature_data = []
@@ -176,7 +210,7 @@ def __format_feature(feature_dic):
     return feature_data
 
 #generate doxyfile for specific soc
-def __generate_doxyfile(chip_name, function_type, input_data, feature_data):
+def __generate_doxyfile(chip_name, input_data, image_path, feature_data):
     
     if function_type == 'rm':
         file_doxyfile_template = 'Doxyfile_lib_PDF_RM'
@@ -197,15 +231,19 @@ def __generate_doxyfile(chip_name, function_type, input_data, feature_data):
     # Define two flags for targeting the PREDEFINED lines
     flag_find_predefined = False
     flag_find_end_predefined = False
+    # Define two flags for targeting the PREDEFINED lines
+    flag_find_image_path = False
+    flag_find_end_image_path = False
     
     # Any time the fixed INPUT changed from Doxyfile Template, this also needs an update.
     fixdir_input = '                         ./ \\\n                         ../common/trademarks/trademarks_template.md \\\n                         mainpage.md \\\n                         architectural_overview.md\n'
+    fixdir_image = '                         ../common/trademarks/FS_COLOR_LOGOSM_JPG.jpg \\\n                         ../common/trademarks/arm_logo.png \\\n                         ../common/trademarks/EES-FSL.png \\\n                         ../../mk/doxygen \\\n                         ./ \\\n                         Kinetis_SDK_Block_Diagram.png \\\n                         kpsdk_guide01.png \\\n                         ../../../platform/drivers/src/tsi/doxygen'
     
     f_file_doxyfile = open(target_doxyfile, 'w')
     for i, line in enumerate(lines):
         # INPUT
         if not flag_find_input:
-    	    if re.search(r'INPUT                  = ', line, re.I) != None:
+            if re.search(r'INPUT                  = ', line, re.I) != None:
                 for data in input_data:
                     f_file_doxyfile.write(data + ' \\\n')
                 f_file_doxyfile.write(fixdir_input)
@@ -218,6 +256,23 @@ def __generate_doxyfile(chip_name, function_type, input_data, feature_data):
                     continue
                 else:
                     flag_find_end_input = True
+                    f_file_doxyfile.write(line)
+                    continue
+        # IMAGE_PATH
+        if not flag_find_image_path:
+            if re.search(r'IMAGE_PATH             = ', line, re.I) != None:
+                for data in image_path:
+                    f_file_doxyfile.write(data + ' \\\n')
+                f_file_doxyfile.write(fixdir_image)
+                flag_find_image_path = True
+                continue
+        if flag_find_image_path:
+            if not flag_find_end_image_path:
+                if not line.strip() == "":
+                    f_file_doxyfile.write("")
+                    continue
+                else:
+                    flag_find_end_image_path = True
                     f_file_doxyfile.write(line)
                     continue
         # PREDEFINED
@@ -255,7 +310,7 @@ if __name__ == '__main__':
     
     # Reads the tool options
     if the_args.chipname == None:
-        print 'You need to give a chip name for this Doxyfile generaion.\nThe name can distinguish itself from a next time generaion, such as python doxyfile_generator2.py -c MK10D10'
+        print 'You need to give a chip name for this Doxyfile generaion.\nThe name can distinguish itself from a next time generaion, such as python doxyfile_generator2.py -c MK63F12'
         sys.exit()
     else:
         chip_name = the_args.chipname
@@ -284,18 +339,18 @@ if __name__ == '__main__':
     #Process feature.h 
     feature_dic = featureParser.get_feature_dic(featurefile_list)
     #Process components.yml
-    components_list = __get_components_list(components_files_list)
+    components_list, middleware_list = __get_components_list(components_files_list)
     #Process driver yml, like: adc.yml
-    components_dir_list = __process_components_list(chip_name, components_list)
+    components_dir_list = __process_components_list(chip_name, components_list, middleware_list)
     #Add other useful information, like:clock.dox, power.dox, utilities and middleware
     all_inputdata = __add_input_information(chip_name, components_dir_list, feature_dic)
     #Format the input information
-    input_data = __format_input(all_inputdata)
+    input_data, image_path = __format_input(all_inputdata)
     #Format the feature information
     feature_data = __format_feature(feature_dic)
     
     #Generate doxyfile
-    doxyfile = __generate_doxyfile(chip_name, function_type, input_data, feature_data)
+    doxyfile = __generate_doxyfile(chip_name, input_data, image_path, feature_data)
     
     print '\nGenerate doxyfile sucessed!'
     
